11fb52cb7bb15de59d970d1fd12d35604a0dbf7a
cw <chrisway613@gmail.com>
2024-03-09 07:26:47 +0000

    add some comments for main.py

--- main.py
+++ main.py
@@ -15,18 +15,20 @@
 
 """Training and evaluation"""
 
-import run_lib
-from absl import app
-from absl import flags
-from ml_collections.config_flags import config_flags
-import logging
 import os
+import logging
+
 import tensorflow as tf
+
+from absl import app, flags
+from ml_collections.config_flags import config_flags
+
+import run_lib
+
 
 FLAGS = flags.FLAGS
 
-config_flags.DEFINE_config_file(
-  "config", None, "Training configuration.", lock_config=True)
+config_flags.DEFINE_config_file("config", None, "Training configuration.", lock_config=True)
 flags.DEFINE_string("workdir", None, "Work directory.")
 flags.DEFINE_enum("mode", None, ["train", "eval"], "Running mode: train or eval")
 flags.DEFINE_string("eval_folder", "eval",
@@ -38,15 +40,18 @@ def main(argv):
   if FLAGS.mode == "train":
     # Create the working directory
     tf.io.gfile.makedirs(FLAGS.workdir)
+    
     # Set logger so that it outputs to both console and file
     # Make logging work for both disk and Google Cloud Storage
     gfile_stream = open(os.path.join(FLAGS.workdir, 'stdout.txt'), 'w')
     handler = logging.StreamHandler(gfile_stream)
     formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
     handler.setFormatter(formatter)
+    
     logger = logging.getLogger()
     logger.addHandler(handler)
     logger.setLevel('INFO')
+    
     # Run the training pipeline
     run_lib.train(FLAGS.config, FLAGS.workdir)
   elif FLAGS.mode == "eval":
