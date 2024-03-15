c673408f99724d38a4e7bf557ecd21aa8c7529fb
cw <chrisway613@gmail.com>
2024-03-15 06:37:26 +0000

    complete comments for sampling.py

--- sde_lib.py
+++ sde_lib.py
@@ -7,330 +7,337 @@ import numpy as np
 
 
 class SDE(abc.ABC):
-  """SDE abstract class. Functions are designed for a mini-batch of inputs."""
+    """SDE abstract class. Functions are designed for a mini-batch of inputs."""
 
-  def __init__(self, N):
-    """Construct an SDE.
+    def __init__(self, N):
+        """Construct an SDE.
 
-    Args:
-      N: number of discretization time steps.
-    """
-    
-    super().__init__()
-    self.N = N
+        Args:
+          N: number of discretization time steps.
+        """
 
-  @property
-  @abc.abstractmethod
-  def T(self):
-    """End time of the SDE."""
-    pass
-
-  @abc.abstractmethod
-  def sde(self, x, t):
-    """Return the drift & diffusion of SDE."""
-    pass
-
-  @abc.abstractmethod
-  def marginal_prob(self, x, t):
-    """Parameters to determine the marginal distribution of the SDE, $p_t(x)$."""
-    pass
-
-  @abc.abstractmethod
-  def prior_sampling(self, shape):
-    """Generate one sample from the prior distribution, $p_T(x)$."""
-    pass
-
-  @abc.abstractmethod
-  def prior_logp(self, z):
-    """Compute log-density of the prior distribution.
-
-    Useful for computing the log-likelihood via probability flow ODE.
-
-    Args:
-      z: latent code
-    Returns:
-      log probability density
-    """
-    pass
-
-  def discretize(self, x, t):
-    """Discretize the SDE in the form: x_{i+1} = x_i + f_i(x_i) + G_i z_i.
-
-    Useful for reverse diffusion sampling and probabiliy flow sampling.
-    Defaults to Euler-Maruyama discretization.
-
-    Args:
-      x: a torch tensor
-      t: a torch float representing the time step (from 0 to `self.T`)
-
-    Returns:
-      f, G
-    """
-    
-    # $\Delta t$
-    dt = 1 / self.N
-    # $f(x,t), G(t)$
-    drift, diffusion = self.sde(x, t)
-    
-    # $f(x,t) \Delta t$
-    f = drift * dt
-    # $G(t) \sqrt{\Delta t}$
-    G = diffusion * torch.sqrt(torch.tensor(dt, device=t.device))
-    
-    return f, G
-
-  def reverse(self, score_fn, probability_flow=False):
-    """Create the reverse-time SDE/ODE.
-
-    Args:
-      score_fn: A time-dependent score-based model that takes x and t and returns the score.
-      probability_flow: If `True`, create the reverse-time ODE used for probability flow sampling. Default: False.
-    
-    Returns:
-      An RSDE object.
-    """
-    
-    N = self.N
-    T = self.T
-    sde_fn = self.sde
-    discretize_fn = self.discretize
-
-    # Build the class for reverse-time SDE.
-    class RSDE(self.__class__):
-      def __init__(self):
+        super().__init__()
         self.N = N
-        self.probability_flow = probability_flow
 
-      @property
-      def T(self):
-        return T
+    @property
+    @abc.abstractmethod
+    def T(self):
+        """End time of the SDE."""
+        pass
 
-      def sde(self, x, t):
-        """Create the drift and diffusion functions for the reverse SDE/ODE."""
-        
-        drift, diffusion = sde_fn(x, t)
-        score = score_fn(x, t)
-        
-        # reverse-sde: $f(x,t) - G^2(t) * s$; ode: $f(x,t) - \frac{1}{2} G^2(t) * s$
-        drift = drift - diffusion[:, None, None, None] ** 2 * score * (0.5 if self.probability_flow else 1.)
-        # Set the diffusion function to zero for ODEs.
-        diffusion = 0. if self.probability_flow else diffusion
-        
-        return drift, diffusion
+    @abc.abstractmethod
+    def sde(self, x, t):
+        """Return the drift & diffusion of SDE."""
+        pass
 
-      def discretize(self, x, t):
-        """Create discretized iteration rules for the reverse diffusion sampler."""
-        
-        score = score_fn(x, t)
-        f, G = discretize_fn(x, t)
-        
-        # sde: $[f(x,t) - G^2(t) * s] \Delta t$; ode: $[f(x,t) - \frac{1}{2} G^2(t) * s] \Delta t$
-        rev_f = f - G[:, None, None, None] ** 2 * score * (0.5 if self.probability_flow else 1.)
-        rev_G = torch.zeros_like(G) if self.probability_flow else G
-        
-        return rev_f, rev_G
+    @abc.abstractmethod
+    def marginal_prob(self, x, t):
+        """Parameters to determine the marginal distribution of the SDE, $p_t(x)$."""
+        pass
 
-    return RSDE()
+    @abc.abstractmethod
+    def prior_sampling(self, shape):
+        """Generate one sample from the prior distribution, $p_T(x)$."""
+        pass
+
+    @abc.abstractmethod
+    def prior_logp(self, z):
+        """Compute log-density of the prior distribution.
+
+        Useful for computing the log-likelihood via probability flow ODE.
+
+        Args:
+          z: latent code
+        Returns:
+          log probability density
+        """
+        pass
+
+    def discretize(self, x, t):
+        """Discretize the SDE in the form: x_{i+1} = x_i + f_i(x_i) + G_i z_i.
+
+        Useful for reverse diffusion sampling and probabiliy flow sampling.
+        Defaults to Euler-Maruyama discretization.
+
+        Args:
+          x: a torch tensor
+          t: a torch float representing the time step (from 0 to `self.T`)
+
+        Returns:
+          f, G
+        """
+
+        # $\Delta t$
+        dt = 1 / self.N
+        # $f(x,t), G(t)$
+        drift, diffusion = self.sde(x, t)
+
+        # $f(x,t) \Delta t$
+        f = drift * dt
+        # $G(t) \sqrt{\Delta t}$
+        G = diffusion * torch.sqrt(torch.tensor(dt, device=t.device))
+
+        return f, G
+
+    def reverse(self, score_fn, probability_flow=False):
+        """Create the reverse-time SDE/ODE.
+
+        Args:
+          score_fn: A time-dependent score-based model that takes x and t and returns the score.
+          probability_flow: If `True`, create the reverse-time ODE used for probability flow sampling. Default: False.
+
+        Returns:
+          An RSDE object.
+        """
+
+        N = self.N
+        T = self.T
+        sde_fn = self.sde
+        discretize_fn = self.discretize
+
+        # Build the class for reverse-time SDE.
+        class RSDE(self.__class__):
+            def __init__(self):
+                self.N = N
+                self.probability_flow = probability_flow
+
+            @property
+            def T(self):
+                return T
+
+            def sde(self, x, t):
+                """Create the drift and diffusion functions for the reverse SDE/ODE."""
+
+                drift, diffusion = sde_fn(x, t)
+                score = score_fn(x, t)
+
+                # reverse-sde: $f(x,t) - G^2(t) * s$; ode: $f(x,t) - \frac{1}{2} G^2(t) * s$
+                drift = drift - diffusion[:, None, None, None] ** 2 * \
+                    score * (0.5 if self.probability_flow else 1.)
+                # Set the diffusion function to zero for ODEs.
+                diffusion = 0. if self.probability_flow else diffusion
+
+                return drift, diffusion
+
+            def discretize(self, x, t):
+                """Create discretized iteration rules for the reverse diffusion sampler."""
+
+                score = score_fn(x, t)
+                f, G = discretize_fn(x, t)
+
+                # sde: $[f(x,t) - G^2(t) * s] \Delta t$; ode: $[f(x,t) - \frac{1}{2} G^2(t) * s] \Delta t$
+                rev_f = f - G[:, None, None, None] ** 2 * \
+                    score * (0.5 if self.probability_flow else 1.)
+                rev_G = torch.zeros_like(G) if self.probability_flow else G
+
+                return rev_f, rev_G
+
+        return RSDE()
 
 
 class VPSDE(SDE):
-  def __init__(self, beta_min=0.1, beta_max=20, N=1000):
-    """Construct a Variance Preserving SDE.
+    def __init__(self, beta_min=0.1, beta_max=20, N=1000):
+        """Construct a Variance Preserving SDE.
 
-    Args:
-      beta_min: value of beta(0)
-      beta_max: value of beta(1)
-      N: number of discretization steps
-    """
-    
-    super().__init__(N)
-    
-    self.N = N
-    
-    self.beta_0 = beta_min
-    self.beta_1 = beta_max
-    self.discrete_betas = torch.linspace(beta_min / N, beta_max / N, N)
-    
-    self.alphas = 1. - self.discrete_betas
-    self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
-    self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
-    self.sqrt_1m_alphas_cumprod = torch.sqrt(1. - self.alphas_cumprod)
+        Args:
+          beta_min: value of beta(0)
+          beta_max: value of beta(1)
+          N: number of discretization steps
+        """
 
-  @property
-  def T(self):
-    return 1
+        super().__init__(N)
 
-  def sde(self, x, t):
-    """Formula (32) in paper."""
-    
-    beta_t = self.beta_0 + t * (self.beta_1 - self.beta_0)
-    # $-\frac{1}{2} \beta(t) x$
-    drift = -0.5 * beta_t[:, None, None, None] * x
-    # $\sqrt{\beta(t)}$
-    diffusion = torch.sqrt(beta_t)
-    
-    return drift, diffusion
+        self.N = N
 
-  def marginal_prob(self, x, t):
-    """Formula (33) in paper."""
-    
-    log_mean_coeff = -0.25 * t ** 2 * (self.beta_1 - self.beta_0) - 0.5 * t * self.beta_0
-    mean = torch.exp(log_mean_coeff[:, None, None, None]) * x
-    std = torch.sqrt(1. - torch.exp(2. * log_mean_coeff))
-    
-    return mean, std
+        self.beta_0 = beta_min
+        self.beta_1 = beta_max
+        self.discrete_betas = torch.linspace(beta_min / N, beta_max / N, N)
 
-  def prior_sampling(self, shape):
-    """Sampling from prior."""
-    
-    return torch.randn(*shape)
+        self.alphas = 1. - self.discrete_betas
+        self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
+        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
+        self.sqrt_1m_alphas_cumprod = torch.sqrt(1. - self.alphas_cumprod)
 
-  def prior_logp(self, z):
-    """Log probability of prior."""
-    
-    shape = z.shape
-    N = np.prod(shape[1:])
-    # 多维标准正态分布的对数概率密度
-    logps = -N / 2. * np.log(2 * np.pi) - torch.sum(z ** 2, dim=(1, 2, 3)) / 2.
-    
-    return logps
+    @property
+    def T(self):
+        return 1
 
-  def discretize(self, x, t):
-    """DDPM discretization."""
-    
-    timestep = (t * (self.N - 1) / self.T).long()
-    
-    beta = self.discrete_betas.to(x.device)[timestep]
-    alpha = self.alphas.to(x.device)[timestep]
-    sqrt_beta = torch.sqrt(beta)
-    
-    # 对应论文公式(10)
-    f = torch.sqrt(alpha)[:, None, None, None] * x - x
-    G = sqrt_beta
-    
-    return f, G
+    def sde(self, x, t):
+        """Formula (32) in paper."""
+
+        beta_t = self.beta_0 + t * (self.beta_1 - self.beta_0)
+        # $-\frac{1}{2} \beta(t) x$
+        drift = -0.5 * beta_t[:, None, None, None] * x
+        # $\sqrt{\beta(t)}$
+        diffusion = torch.sqrt(beta_t)
+
+        return drift, diffusion
+
+    def marginal_prob(self, x, t):
+        """Formula (33) in paper."""
+
+        log_mean_coeff = -0.25 * t ** 2 * \
+            (self.beta_1 - self.beta_0) - 0.5 * t * self.beta_0
+        mean = torch.exp(log_mean_coeff[:, None, None, None]) * x
+        std = torch.sqrt(1. - torch.exp(2. * log_mean_coeff))
+
+        return mean, std
+
+    def prior_sampling(self, shape):
+        """Sampling from prior."""
+
+        return torch.randn(*shape)
+
+    def prior_logp(self, z):
+        """Log probability of prior."""
+
+        shape = z.shape
+        N = np.prod(shape[1:])
+        # 多维标准正态分布的对数概率密度
+        logps = -N / 2. * np.log(2 * np.pi) - \
+            torch.sum(z ** 2, dim=(1, 2, 3)) / 2.
+
+        return logps
+
+    def discretize(self, x, t):
+        """DDPM discretization."""
+
+        timestep = (t * (self.N - 1) / self.T).long()
+
+        beta = self.discrete_betas.to(x.device)[timestep]
+        alpha = self.alphas.to(x.device)[timestep]
+        sqrt_beta = torch.sqrt(beta)
+
+        # 对应论文公式(10)
+        f = torch.sqrt(alpha)[:, None, None, None] * x - x
+        G = sqrt_beta
+
+        return f, G
 
 
 class subVPSDE(SDE):
-  def __init__(self, beta_min=0.1, beta_max=20, N=1000):
-    """Construct the sub-VP SDE that excels at likelihoods.
+    def __init__(self, beta_min=0.1, beta_max=20, N=1000):
+        """Construct the sub-VP SDE that excels at likelihoods.
 
-    Args:
-      beta_min: value of beta(0)
-      beta_max: value of beta(1)
-      N: number of discretization steps
-    """
-    
-    super().__init__(N)
-    
-    self.N = N
-    self.beta_0 = beta_min
-    self.beta_1 = beta_max
+        Args:
+          beta_min: value of beta(0)
+          beta_max: value of beta(1)
+          N: number of discretization steps
+        """
 
-  @property
-  def T(self):
-    return 1
+        super().__init__(N)
 
-  def sde(self, x, t):
-    """Formula (12) in paper."""
-    
-    beta_t = self.beta_0 + t * (self.beta_1 - self.beta_0)
-    drift = -0.5 * beta_t[:, None, None, None] * x
-    # 将 $\beta(t) = \beta_0 + t(\beta_1 - \beta_0)$ 代入式(12)中计算自然底数的幂部分
-    discount = 1. - torch.exp(-2 * self.beta_0 * t - (self.beta_1 - self.beta_0) * t ** 2)
-    diffusion = torch.sqrt(beta_t * discount)
-    
-    return drift, diffusion
+        self.N = N
+        self.beta_0 = beta_min
+        self.beta_1 = beta_max
 
-  def marginal_prob(self, x, t):
-    """Formula (34) in paper."""
-    
-    log_mean_coeff = -0.25 * t ** 2 * (self.beta_1 - self.beta_0) - 0.5 * t * self.beta_0
-    mean = torch.exp(log_mean_coeff)[:, None, None, None] * x
-    std = 1 - torch.exp(2. * log_mean_coeff)
-    
-    return mean, std
+    @property
+    def T(self):
+        return 1
 
-  def prior_sampling(self, shape):
-    """Sampling from prior."""
-    
-    return torch.randn(*shape)
+    def sde(self, x, t):
+        """Formula (12) in paper."""
 
-  def prior_logp(self, z):
-    """Log probability of prior."""
-    
-    shape = z.shape
-    N = np.prod(shape[1:])
-    
-    # 多维标准正态分布的对数概率密度
-    return -N / 2. * np.log(2 * np.pi) - torch.sum(z ** 2, dim=(1, 2, 3)) / 2.
+        beta_t = self.beta_0 + t * (self.beta_1 - self.beta_0)
+        drift = -0.5 * beta_t[:, None, None, None] * x
+        # 将 $\beta(t) = \beta_0 + t(\beta_1 - \beta_0)$ 代入式(12)中计算自然底数的幂部分
+        discount = 1. - torch.exp(-2 * self.beta_0 *
+                                  t - (self.beta_1 - self.beta_0) * t ** 2)
+        diffusion = torch.sqrt(beta_t * discount)
+
+        return drift, diffusion
+
+    def marginal_prob(self, x, t):
+        """Formula (34) in paper."""
+
+        log_mean_coeff = -0.25 * t ** 2 * \
+            (self.beta_1 - self.beta_0) - 0.5 * t * self.beta_0
+        mean = torch.exp(log_mean_coeff)[:, None, None, None] * x
+        std = 1 - torch.exp(2. * log_mean_coeff)
+
+        return mean, std
+
+    def prior_sampling(self, shape):
+        """Sampling from prior."""
+
+        return torch.randn(*shape)
+
+    def prior_logp(self, z):
+        """Log probability of prior."""
+
+        shape = z.shape
+        N = np.prod(shape[1:])
+
+        # 多维标准正态分布的对数概率密度
+        return -N / 2. * np.log(2 * np.pi) - torch.sum(z ** 2, dim=(1, 2, 3)) / 2.
 
 
 class VESDE(SDE):
-  def __init__(self, sigma_min=0.01, sigma_max=50, N=1000):
-    """Construct a Variance Exploding SDE.
+    def __init__(self, sigma_min=0.01, sigma_max=50, N=1000):
+        """Construct a Variance Exploding SDE.
 
-    Args:
-      sigma_min: smallest sigma.
-      sigma_max: largest sigma.
-      N: number of discretization steps
-    """
-    
-    super().__init__(N)
-    
-    self.N = N
-    
-    self.sigma_min = sigma_min
-    self.sigma_max = sigma_max
-    # 幂成等差数列, 最终便是等比数列
-    self.discrete_sigmas = torch.exp(torch.linspace(np.log(self.sigma_min), np.log(self.sigma_max), N))
+        Args:
+          sigma_min: smallest sigma.
+          sigma_max: largest sigma.
+          N: number of discretization steps
+        """
 
-  @property
-  def T(self):
-    return 1
+        super().__init__(N)
 
-  def sde(self, x, t):
-    """Formula (30) in paper."""
-    
-    sigma = self.sigma_min * (self.sigma_max / self.sigma_min) ** t
-    drift = torch.zeros_like(x)
-    diffusion = sigma * torch.sqrt(torch.tensor(2 * (np.log(self.sigma_max) - np.log(self.sigma_min)),
-                                                device=t.device))
-    return drift, diffusion
+        self.N = N
 
-  def marginal_prob(self, x, t):
-    """Formula (31) in paper."""
-    
-    std = self.sigma_min * (self.sigma_max / self.sigma_min) ** t
-    mean = x
-    
-    return mean, std
+        self.sigma_min = sigma_min
+        self.sigma_max = sigma_max
+        # 幂成等差数列, 最终便是等比数列
+        self.discrete_sigmas = torch.exp(torch.linspace(
+            np.log(self.sigma_min), np.log(self.sigma_max), N))
 
-  def prior_sampling(self, shape):
-    """Sampling from prior: \mathcal N(0, sigma_max I)"""
-    
-    return torch.randn(*shape) * self.sigma_max
+    @property
+    def T(self):
+        return 1
 
-  def prior_logp(self, z):
-    """Log probability of prior."""
-    
-    shape = z.shape
-    N = np.prod(shape[1:])
-    
-    return -N / 2. * np.log(2 * np.pi * self.sigma_max ** 2) - torch.sum(z ** 2, dim=(1, 2, 3)) / (2 * self.sigma_max ** 2)
+    def sde(self, x, t):
+        """Formula (30) in paper."""
 
-  def discretize(self, x, t):
-    """SMLD(NCSN) discretization."""
-    
-    timestep = (t * (self.N - 1) / self.T).long()
-    sigma = self.discrete_sigmas.to(t.device)[timestep]
-    # $\sigma_{i-1}, \sigma_0 = 0$ 
-    adjacent_sigma = torch.where(timestep == 0, torch.zeros_like(t),
-                                 self.discrete_sigmas[timestep - 1].to(t.device))
-    
-    # 对应论文公式(8)
-    f = torch.zeros_like(x)
-    # $\sigma_i^2 - \sigma_{i-1}^2$
-    G = torch.sqrt(sigma ** 2 - adjacent_sigma ** 2)
-    
-    return f, G
+        sigma = self.sigma_min * (self.sigma_max / self.sigma_min) ** t
+        drift = torch.zeros_like(x)
+        diffusion = sigma * torch.sqrt(torch.tensor(2 * (np.log(self.sigma_max) - np.log(self.sigma_min)),
+                                                    device=t.device))
+        return drift, diffusion
+
+    def marginal_prob(self, x, t):
+        """Formula (31) in paper."""
+
+        std = self.sigma_min * (self.sigma_max / self.sigma_min) ** t
+        mean = x
+
+        return mean, std
+
+    def prior_sampling(self, shape):
+        """Sampling from prior: \mathcal N(0, sigma_max I)"""
+
+        return torch.randn(*shape) * self.sigma_max
+
+    def prior_logp(self, z):
+        """Log probability of prior."""
+
+        shape = z.shape
+        N = np.prod(shape[1:])
+
+        return -N / 2. * np.log(2 * np.pi * self.sigma_max ** 2) - torch.sum(z ** 2, dim=(1, 2, 3)) / (2 * self.sigma_max ** 2)
+
+    def discretize(self, x, t):
+        """SMLD(NCSN) discretization."""
+
+        timestep = (t * (self.N - 1) / self.T).long()
+        sigma = self.discrete_sigmas.to(t.device)[timestep]
+        # $\sigma_{i-1}, \sigma_0 = 0$
+        adjacent_sigma = torch.where(timestep == 0, torch.zeros_like(t),
+                                     self.discrete_sigmas[timestep - 1].to(t.device))
+
+        # 对应论文公式(8)
+        f = torch.zeros_like(x)
+        # $\sigma_i^2 - \sigma_{i-1}^2$
+        G = torch.sqrt(sigma ** 2 - adjacent_sigma ** 2)
+
+        return f, G
